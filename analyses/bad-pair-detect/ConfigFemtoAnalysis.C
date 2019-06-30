20a394a15293bca70eb0fb709051b078bec1567d
Andrew Kubera <andrew.kubera@floss.email>
2019-06-30 13:23:59 -0400

    bad-pair-detect: Add differential cut

--- analyses/bad-pair-detect/ConfigFemtoAnalysis.C
+++ analyses/bad-pair-detect/ConfigFemtoAnalysis.C
@@ -65,18 +65,203 @@ struct PseudoEsdTrackCut : public AliFemtoTrackCutPionPionIdealAK, public AliFem
   PseudoEsdTrackCut(const AliFemtoESDTrackCut &cut)
     : AliFemtoTrackCutPionPionIdealAK()
     , AliFemtoESDTrackCut(cut)
-    {}
+    {
+      max_xy = fMaxImpactXY;
+      max_z = fMaxImpactZ;
+      pt_range = {fPt[0], fPt[1]};
+      nsigma_pion = fNsigma;
+      rchi2_tpc_max = fMaxTPCchiNdof;
+      // rchi2_its_max = fMaxITSchiNdof;
+      rchi2_its_max = 1.6;
+    }
 
   virtual ~PseudoEsdTrackCut() {}
 
   virtual bool Pass(const AliFemtoTrack *track)
     {
-      const AliFemtoModelHiddenInfo *info = static_cast<AliFemtoModelHiddenInfo*>(track->GetHiddenInfo());
-      const Int_t origin = info->GetOrigin();
-      if (origin != 0) { // } || std::fabs(info->GetMass() - 0.139) > .001) {
+      // const AliFemtoModelHiddenInfo *info = static_cast<AliFemtoModelHiddenInfo*>(track->GetHiddenInfo());
+      // const Int_t origin = info->GetOrigin();
+      // if (origin != 0) { // } || std::fabs(info->GetMass() - 0.139) > .001) {
+      //   return false;
+      // }
+
+      if (!AliFemtoTrackCutPionPionAK::Pass(track)) {
         return false;
       }
-      return AliFemtoESDTrackCut::Pass(track);
+
+      if (!Pass1(track)) {
+        return false;
+      }
+
+      // if (!Pass2(track)) {
+      //   return false;
+      // }
+
+      // if (!Pass3(track)) {
+      //   return false;
+      // }
+
+      fNTracksPassed++;
+      return true;
+      // return AliFemtoESDTrackCut::Pass(track);
+    }
+
+  bool Pass1(const AliFemtoTrack *track)
+    {
+        if (fStatus && (track->Flags() & fStatus) != fStatus) {
+          return false;
+        }
+        if (fRemoveKinks && (track->KinkIndex(0) || track->KinkIndex(1) || track->KinkIndex(2))) {
+          return false;
+        }
+        if (fRemoveITSFake && track->ITSncls() < 0) {
+          return false;
+        }
+        if (fminTPCclsF > track->TPCnclsF()) {
+          return false;
+        }
+        if (fminTPCncls > track->TPCncls()) {
+          return false;
+        }
+        if (fminITScls > track->ITSncls()) {
+          return false;
+        }
+
+        if (fMaxImpactXY < TMath::Abs(track->ImpactD())) {
+          return false;
+        }
+        if (fMinImpactXY > TMath::Abs(track->ImpactD())) {
+          return false;
+        }
+        if (fMaxImpactZ < TMath::Abs(track->ImpactZ())) {
+          return false;
+        }
+        if (fMaxSigmaToVertex < track->SigmaToVertex()) {
+          return false;
+        }
+
+      /*
+        if (track->ITSncls() > 0 && (track->ITSchi2() / track->ITSncls()) > fMaxITSchiNdof) {
+          return false;
+        }
+        */
+
+        if (track->TPCchi2perNDF() > fMaxTPCchiNdof) {
+          return false;
+        }
+
+        // ITS cluster requirenments
+        for (Int_t i = 0; i < 3; i++) {
+          if (!CheckITSClusterRequirement(fCutClusterRequirementITS[i], track->HasPointOnITSLayer(i * 2), track->HasPointOnITSLayer(i*2+1))) {
+            return false;
+          }
+        }
+
+        if (fLabel) {
+          if (track->Label() < 0) {
+            fNTracksFailed++;
+            return false;
+          }
+        }
+        if (fCharge != 0 && (track->Charge() != fCharge)) {
+          fNTracksFailed++;
+          return false;
+        }
+
+
+        Bool_t tTPCPidIn = (track->Flags() & AliFemtoTrack::kTPCpid) > 0;
+        Bool_t tITSPidIn = (track->Flags() & AliFemtoTrack::kITSpid) > 0;
+        Bool_t tTOFPidIn = (track->Flags() & AliFemtoTrack::kTOFpid) > 0;
+
+        const double momentum = track->P().Mag();
+
+        if (fMinPforTOFpid > 0
+            && fMinPforTOFpid < momentum && momentum < fMaxPforTOFpid
+            && !tTOFPidIn) {
+          fNTracksFailed++;
+          return false;
+        }
+
+        if (fMinPforTPCpid > 0
+            && fMinPforTPCpid < momentum && momentum < fMaxPforTPCpid
+            && !tTPCPidIn) {
+          fNTracksFailed++;
+          return false;
+        }
+
+        if (fMinPforITSpid > 0
+            && fMinPforITSpid < momentum && momentum < fMaxPforITSpid
+            && !tITSPidIn) {
+          fNTracksFailed++;
+          return false;
+        }
+
+        float tEnergy = track->P().MassHypothesis(static_cast<AliFemtoESDTrackCut*>(this)->Mass());
+        float tRapidity = 0;
+        if (tEnergy-track->P().z() != 0 && (tEnergy + track->P().z()) / (tEnergy-track->P().z()) > 0)
+          tRapidity = 0.5 * ::log((tEnergy + track->P().z())/(tEnergy-track->P().z()));
+        float tPt = track->P().Perp();
+        float tEta = track->P().PseudoRapidity();
+
+        if (fMaxImpactXYPtOff < 999.0) {
+          if ((fMaxImpactXYPtOff + fMaxImpactXYPtNrm*TMath::Power(tPt, fMaxImpactXYPtPow)) < TMath::Abs(track->ImpactD())) {
+            fNTracksFailed++;
+            return false;
+          }
+        }
+
+        if ((tRapidity < fRapidity[0]) || (tRapidity > fRapidity[1])) {
+          fNTracksFailed++;
+          return false;
+        }
+        if ((tEta < fEta[0]) || (tEta > fEta[1])) {
+          fNTracksFailed++;
+          return false;
+        }
+        if ((tPt < fPt[0]) || (tPt > fPt[1])) {
+          fNTracksFailed++;
+          return false;
+        }
+
+      return true;
+    }
+
+  bool Pass2(const AliFemtoTrack *track)
+    {
+      if ((track->PidProbElectron() < fPidProbElectron[0]) || (track->PidProbElectron() > fPidProbElectron[1])) {
+        fNTracksFailed++;
+        return false;
+      }
+      if ((track->PidProbPion() < fPidProbPion[0]) || (track->PidProbPion() > fPidProbPion[1])) {
+        fNTracksFailed++;
+        return false;
+      }
+      if ((track->PidProbKaon() < fPidProbKaon[0]) || (track->PidProbKaon() > fPidProbKaon[1])) {
+        fNTracksFailed++;
+        return false;
+      }
+      if ((track->PidProbProton() < fPidProbProton[0]) || (track->PidProbProton() > fPidProbProton[1])) {
+        fNTracksFailed++;
+        return false;
+      }
+      if ((track->PidProbMuon() < fPidProbMuon[0]) || (track->PidProbMuon() > fPidProbMuon[1])) {
+        fNTracksFailed++;
+        return false;
+      }
+      return true;
+    }
+
+  bool Pass3(const AliFemtoTrack *track)
+    {
+      if (fElectronRejection && !IsElectron(track->NSigmaTPCE(),track->NSigmaTPCPi(),track->NSigmaTPCK(), track->NSigmaTPCP())) {
+          return false;
+      }
+
+      if (!IsPionNSigma(track->P().Mag(), track->NSigmaTPCPi(), track->NSigmaTOFPi())) {
+        return false;
+      }
+
+      return true;
     }
 
   virtual const char* ClassName() const
@@ -118,17 +303,13 @@ AddAnalysis(TString name, AFAPP::AnalysisParams a, AFAPP::CutParams c, AliFemtoM
   // c.cuts_use_attrs = false;
   // auto *track_cut = analysis->BuildPionCut1(c);
 
-  c.cuts_use_attrs = false;
-  auto *track_cut = static_cast<AliFemtoESDTrackCut *>(analysis->BuildPionCut1(c));
-  track_cut->SetMostProbable(0);
-
   // Use Copy of ESD Track cut
   // c.cuts_use_attrs = false;
   // AliFemtoESDTrackCut *track_cut = new AliFemtoESDTrackCut(static_cast<const AliFemtoESDTrackCut&>(*analysis->BuildPionCut1(c)));
 
-  // Use Pseudo-Esd Track Cut
-  // c.cuts_use_attrs = false;
-  // AliFemtoESDTrackCut *track_cut = new PseudoEsdTrackCut(static_cast<const AliFemtoESDTrackCut&>(*analysis->BuildPionCut1(c)));
+  // Use Pseudo-ESD Track Cut
+  c.cuts_use_attrs = false;
+  AliFemtoESDTrackCut *track_cut = new PseudoEsdTrackCut(static_cast<const AliFemtoESDTrackCut&>(*analysis->BuildPionCut1(c)));
 
   // Use PrimaryPionCut
   // auto *track_cut = new PrimaryPionCut(*static_cast<AliFemtoTrackCutPionPionIdealAK*>(analysis->FirstParticleCut()));
