c963d9448acff94bd3a9b34ceda2e44a9c9c2529
Andrew Kubera <andrew.kubera@cern.ch>
2019-08-05 01:29:43 -0400

    mrc-hypercube: Add functions to RunAanlysis

--- analyses/mrc-hypercube/ConfigFemtoAnalysis.C
+++ analyses/mrc-hypercube/ConfigFemtoAnalysis.C
@@ -46,19 +46,6 @@ struct PrimaryPionCut : public AliFemtoTrackCutPionPionIdealAK {
     { return "PrimaryPionCut"; }
 };
 
-struct HupPionCut : public AliFemtoESDTrackCut {
-public:
-
-  HupPionCut()
-    : AliFemtoESDTrackCut()
-    {}
-
-  virtual ~HupPionCut()
-    {}
-
-  virtual const char* ClassName() const
-    { return "HupPionCut"; }
-};
 
 static const ULong_t filter_mask = BIT(7);
 // static const ULong_t filter_mask = BIT(8);
@@ -89,7 +76,7 @@ AddAnalysis(TString name, AFAPP::AnalysisParams a, AFAPP::CutParams c, AliFemtoM
 {
   AliFemtoAnalysisPionPion *analysis = new AliFemtoAnalysisPionPion(name, a, c);
   analysis->SetTrackFilter(filter_mask);
-
+/*
   c.cuts_use_attrs = false;
   auto *track_cut = static_cast<AliFemtoESDTrackCut*>(analysis->BuildPionCut1(c));
   c.cuts_use_attrs = true;
@@ -98,37 +85,37 @@ AddAnalysis(TString name, AFAPP::AnalysisParams a, AFAPP::CutParams c, AliFemtoM
   // auto *track_cut = new AliFemtoTrackCutPionPionIdealAK();
   analysis->SetFirstParticleCut(track_cut);
   analysis->SetSecondParticleCut(track_cut);
+*/
 
-  auto *ff = new AliFemtoModelManager();
-  auto *wg = new AliFemtoModelWeightGeneratorBasic();
-  ff->AcceptWeightGenerator(wg);
-
-  auto *mrc_cf_1d = new AliFemtoModelCorrFctn("TrueQinv", 200, 0.0, 1.0);
-  mrc_cf_1d->ConnectToManager(ff);
-
-  auto *ktmrc1d = new AliFemtoKtBinnedCorrFunc("KT_MRC1D", mrc_cf_1d);
-
-  // analysis->AddCorrFctn(mrc_cf_1d);
-
-  auto *mrc_cf = new AliFemtoModelCorrFctnTrueQ6D("MRC",
-                  // 22, 0.0, 0.1125,
-                  // 45, -0.1125, 0.1125,
-                  // 45, -0.1125, 0.1125);
-                  30, 0.0, 0.1525,
-                  61, -0.1525, 0.1525,
-                  61, -0.1525, 0.1525);
-
-  auto *ktmrc = new AliFemtoKtBinnedCorrFunc("KT_HYPERCUBE", mrc_cf);
 #if true
   const std::vector<std::pair<float, float>> ktbins = {{0.2, 0.3}, {0.4, 0.5}, {0.6, 0.7}};
 #else
   const std::vector<std::pair<float, float>> ktbins = {{0.3, 0.4}, {0.5, 0.6}, {0.7, 0.8}, {0.8, 1.0}, {1.0, 1.2}};
 #endif
 
+  auto *ff = new AliFemtoModelManager();
+  auto *wg = new AliFemtoModelWeightGeneratorBasic();
+  ff->AcceptWeightGenerator(wg);
+
+  auto *mrc_cf_1d = new AliFemtoModelCorrFctnQinv("TrueQinv", 200, 0.0, 1.0);
+  mrc_cf_1d->ConnectToManager(ff);
+
+  auto *ktmrc1d = new AliFemtoKtBinnedCorrFunc("KT_MRC1D", mrc_cf_1d);
   ktmrc1d->AddKtRanges(ktbins);
-  ktmrc->AddKtRanges(ktbins);
 
   analysis->AddCorrFctn(ktmrc1d);
+
+  int out_nbins = 32;
+  double out_max = out_nbins * (0.1125 / 22);
+  auto *mrc_cf = new AliFemtoModelCorrFctnTrueQ6D("MRC",
+                  out_nbins, 0, out_max,
+                  61, -0.1525, 0.1525,
+                  61, -0.1525, 0.1525);
+
+  auto *ktmrc = new AliFemtoKtBinnedCorrFunc("KT_HYPERCUBE", mrc_cf);
+
+  ktmrc->AddKtRanges(ktbins);
+
   analysis->AddCorrFctn(ktmrc);
 
   analysis->AddStanardCutMonitors();
@@ -150,10 +137,11 @@ ConfigFemtoAnalysis()
   ccfg.mc_pion_only = true;
   ccfg.pion_1_rm_neg_lbl = true;
   ccfg.pion_1_status = 16;
-  ccfg.pion_1_max_impact_xy = 0.04;
-  ccfg.pion_1_max_impact_z = 0.05;
+  ccfg.pion_1_max_impact_xy = 0.03;
+  ccfg.pion_1_max_impact_z = 0.04;
   ccfg.pion_1_sigma = 3.0;
-  ccfg.pion_1_max_its_chi_ndof = 1.6;
+  ccfg.pion_1_min_its_ncls = 3;
+  ccfg.pion_1_max_its_chi_ndof = 2.5;
   ccfg.pion_1_max_tpc_chi_ndof = 1.6;
   ccfg.pion_1_min_tpc_chi_ndof = 0.33;
   ccfg.event_CentralityMin = 0.0;
--- analyses/mrc-hypercube/RunAnalysis.C
+++ analyses/mrc-hypercube/RunAnalysis.C
@@ -2,18 +2,35 @@
 /// \file RunAnalysis.C
 ///
 
+#include <TROOT.h>
+#include <TSystem.h>
+#include <TInterpreter.h>
+#include <TApplication.h>
 #include <TString.h>
 #include <TDatime.h>
 #include <TChain.h>
 #include <TStopwatch.h>
+#include <TPython.h>
 
 #include <AliAnalysisAlien.h>
 #include <AliAnalysisManager.h>
+#include <AliAnalysisTaskFemtoNu.h>
 
 #include <set>
 #include <string>
 #include <fstream>
 #include <iostream>
+
+#define ALIPHYSICS_VERSION "vAN-20190803_ROOT6-1"
+
+
+void usage(std::ostream &out)
+{
+   out <<    "usage: RunAnalysis.C <mode> <options>\n"
+       << "\n"
+       << "\n    mode:"
+       << "\n";
+}
 
 
 std::set<TString>
@@ -33,33 +50,34 @@ load_files(std::string filename)
 
 
 void
-setup_grid(AliAnalysisManager *mgr, TString workdir)
+setup_grid(AliAnalysisManager *mgr, TString workdir, TString gridmode)
 {
   auto *alien = new AliAnalysisAlien();
-  alien->SetRunMode("full");
-  //alien->SetRunMode("terminate");
+  alien->SetRunMode(gridmode);
   alien->SetGridOutputDir("output");
   alien->SetGridWorkingDir(workdir);
-  alien->SetAliPhysicsVersion("vAN-20190705_ROOT6-1");
+  alien->SetAliPhysicsVersion(ALIPHYSICS_VERSION);
   alien->SetDropToShell(false);
   alien->SetCheckCopy(false);
   alien->SetMaxMergeFiles(7);
-  alien->SetMaxMergeStages(30);
+  alien->SetMaxMergeStages(9);
   alien->SetSplitMaxInputFileNumber(15);
   alien->SetNrunsPerMaster(30);
   alien->SetMergeViaJDL(true);
   alien->SetTTL(12 * 60 * 60);
   alien->AddAdditionalLibrary("ConfigFemtoAnalysis.C");
 
-  alien->SetGridDataDir("/alice/sim/2016/LHC16i3a");
-  alien->SetDataPattern("/AOD/*/AliAOD.root");
+  alien->SetGridDataDir("/alice/sim/2016/LHC16g1");
+  alien->SetDataPattern("/AOD198/*/AliAOD.root");
 
   std::set<int> runs = {
-  // 246675, 246676, 246805, 246804, 246807, 246424, 246808,
-  // 246809, 246428, 246431, 246945, 246434,
-  246948, 246844, 246845, 246846, 246847, 246851, 246980, 246982, 246984, 246989, 246991, 246865,
-  // 246994, 246867, 246487, 246871, 246488, 246493, 246750, 246751, 246495, 246757, 246758, 246759,
-  // 246760, 246763, 246765
+    // negfield
+    246945, 246434, 246948, 246844, 246845, 246846, 246847, 246851, 246980, 246982,
+    246984, 246989, 246991, 246865, 246994, 246867, 246487, 246871, 246488, 246493,
+
+    // posfield
+    // 245683, 245692, 245949, 245952, 245954, 245700, 245829, 246087, 245831, 245705,
+    // 246089, 246217, 245833, 245963, 246222, 246225, 246113, 245729, 245731, 246115,
   };
 
   for (auto run : runs) {
@@ -73,32 +91,15 @@ setup_grid(AliAnalysisManager *mgr, TString workdir)
 }
 
 
-void
-RunAnalysis(TString wd="")
-{
-  std::cout << "RunAnalysis\n";
 
-  TStopwatch timer;
-  timer.Start();
+AliAnalysisManager*
+CreateRunManager(TString filename)
+{
+  auto *mgr = new AliAnalysisManager();
 
   TDatime td;
   TString timestamp = Form("%08d%06d", td.GetDate(), td.GetTime());
-
-  auto *mgr = new AliAnalysisManager();
-
-  if (wd.IsWhitespace()) {
-    wd = "job-" + timestamp;
-  } else {
-    timestamp = wd(wd.Index('-')+1, 14);
-  }
-
-  // std::cout << timestamp << "\n" << wd << "\n";
-  gSystem->mkdir(wd);
-  gSystem->CopyFile("ConfigFemtoAnalysis.C", wd + "/ConfigFemtoAnalysis.C");
-  gSystem->cd(wd);
-
-  TString output_filename = Form("MrcResult-%s.root", timestamp.Data());
-  mgr->SetCommonFileName(output_filename);
+  mgr->SetCommonFileName(filename);
 
   gROOT->Macro("$ALICE_ROOT/ANALYSIS/macros/train/AddAODHandler.C");
   gROOT->Macro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C(kTRUE, kTRUE, kTRUE)");
@@ -108,8 +109,64 @@ RunAnalysis(TString wd="")
   mgr->AddTask(femtotask);
   femtotask->SetupContainers();
 
+  return mgr;
+}
 
+
+AliAnalysisManager*
+CreateMergeManager(TString filename)
+{
+  auto *mgr = new AliAnalysisManager();
+  mgr->SetCommonFileName(filename);
+
+  gROOT->Macro("$ALICE_ROOT/ANALYSIS/macros/train/AddAODHandler.C");
+  gROOT->Macro("$ALICE_ROOT/ANALYSIS/macros/AddTaskPIDResponse.C(kTRUE, kTRUE, kTRUE)");
+
+  return mgr;
+}
+
+
+TString result_filename_from_wd(const TString wd)
+{
+  TString timestamp = wd(wd.Index('-')+1, 14);
+  TString result = Form("MrcResult-%s.root", timestamp.Data());
+  return result;
+}
+
+void
+RunGridAnalysis(TString wd)
+{
+  const TString result_filename = result_filename_from_wd(wd);
+
+  auto *mgr = CreateRunManager(result_filename);
+
+  gSystem->mkdir(wd);
+  gSystem->CopyFile("ConfigFemtoAnalysis.C", wd + "/ConfigFemtoAnalysis.C");
+  gSystem->cd(wd);
+
+  setup_grid(mgr, wd, "full");
+
+  mgr->InitAnalysis();
+  mgr->PrintStatus();
+  mgr->StartAnalysis("grid");
+}
+
+
+void
+RunMergeAnalysis(TString wd)
+{
+  const TString result_filename = result_filename_from_wd(wd);
+  auto *mgr = CreateMergeManager(result_filename);
+
+  setup_grid(mgr, wd, "terminate");
+
+  mgr->InitAnalysis();
+  mgr->PrintStatus();
+  mgr->StartAnalysis("grid");
+
+/*
 #define RUN_GRID true
+
 
 #if RUN_GRID
   setup_grid(mgr, wd);
@@ -137,4 +194,76 @@ RunAnalysis(TString wd="")
   TString outfile = wd + "/" + mgr->GetCommonFileName();
   std::cout << "Output written to " << outfile << "\n";
 #endif
+*/
+}
+
+
+void
+RunLocalAnalysis(TString wd)
+{
+}
+
+
+void
+RunAnalysis(std::vector<std::string> args)
+{
+  if (args.size() < 1) {
+    usage(std::cerr);
+    return;
+  }
+
+  if (args[0] == "merge")  {
+    TString wd;
+
+    if (args.size() == 1) {
+      TPython::Exec("from pathlib import Path");
+      wd = (const char*)TPython::Eval("str(list(Path().glob('job-*'))[-1])");
+      std::cout << "Merging latest" << wd <<"\n";
+    } else {
+      wd = args.at(1);
+      std::cout << "Merging: " << wd << "\n";
+    }
+
+    RunMergeAnalysis(wd);
+    return;
+  }
+
+  // create new analysis
+  if (args[0] == "run" || args[0] == "new") {
+
+    TDatime td;
+    const TString
+      timestamp = Form("%08d%06d", td.GetDate(), td.GetTime()),
+      workdir = "job-" + timestamp;
+
+    std::cout << "Creating new job: " << workdir << "\n";
+    RunGridAnalysis(workdir);
+    return;
+  }
+
+  // switch (args.size() - 1) {
+  // case 1:
+  //   RunAnalysis(args[1]);
+  //   break;
+  // case 2:
+  //   // RunAnalysis(args[1], std::stoi(args[2]));
+  //   break;
+  // default:
+    usage(std::cerr);
+  // }
+}
+
+void
+RunAnalysis()
+{
+  char **argv = gApplication->Argv();
+  const int argc = gApplication->Argc();
+
+  std::vector<std::string> all_args(argv, argv+argc);
+
+  std::string macro_name(gInterpreter->GetCurrentMacroName());
+  std::string macro_path(macro_name.begin() + macro_name.find("/./") + 3, macro_name.end());
+
+  std::vector<std::string> args(std::find(all_args.begin(), all_args.end(), macro_path) + 1, all_args.end());
+  RunAnalysis(args);
 }
