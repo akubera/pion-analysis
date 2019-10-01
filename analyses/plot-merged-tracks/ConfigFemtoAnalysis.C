c499628ac09cdc6d60c415dd9dd9e68572fce50b
Andrew Kubera <andrew.kubera@cern.ch>
2019-09-30 20:28:05 -0400

    updated plot-merged-tracks analysis

--- analyses/plot-merged-tracks/ConfigFemtoAnalysis.C
+++ analyses/plot-merged-tracks/ConfigFemtoAnalysis.C
@@ -9,11 +9,53 @@
 #include <AliFemtoCorrFctn.h>
 #include <AliFemtoCorrFctn3DLCMSPosQuad.h>
 
+#include <AliFemtoAnalysisPionPionCuts.h>
+
 #include <sstream>
 
 using AFAPP = AliFemtoAnalysisPionPion;
 
 
+struct PairPlotterCut : public AliFemtoPairCutPionPionAKDetaDphi {
+
+  PairPlotterCut(const AliFemtoPairCutPionPionAKDetaDphi &orig)
+    : AliFemtoPairCutPionPionAKDetaDphi(orig)
+    {
+    }
+
+  bool Pass(const AliFemtoPair *pair) override
+    {
+      const AliFemtoTrack
+        &track1 = *pair->Track1()->Track(),
+        &track2 = *pair->Track2()->Track();
+
+      if (!pwgfemto::PairCutAttrsBaseAK::Pass(*pair)) {
+        return false;
+      }
+
+      if (PairCutTrackAttrDetaDphiStar::Pass(track1, track2)) {
+          std::stringstream s1,
+                            s2;
+
+          for (int i=0; i<9; ++i) {
+            const auto
+              &p1 = track1.NominalTpcPoint(i),
+              &p2 = track2.NominalTpcPoint(i);
+
+            s1 << (i == 0 ? "" : ", ") << Form("[%g, %g, %g]", p1.x(), p1.y(), p1.z());
+            s2 << (i == 0 ? "" : ", ") << Form("[%g, %g, %g]", p2.x(), p2.y(), p2.z());
+          }
+
+        std::cout << "[" << s1.str() << "]\n";
+        std::cout << "[" << s2.str() << "]\n\n";
+
+      }
+
+      return true;
+    }
+};
+
+/*
 struct AliFoo : public AliFemtoCorrFctn {
 
   AliFoo()
@@ -85,6 +127,7 @@ struct AliFoo : public AliFemtoCorrFctn {
     { return new AliFoo(); }
 
 };
+*/
 
 
 void
@@ -125,8 +168,12 @@ AddPionAnalysis(AliFemtoManager &mgr)
   acfg.pion_type_2 = AFAPP::kNone;
 
   auto *analysis = new AliFemtoAnalysisPionPion("pion_analysis_10_20_pip", acfg, ccfg);
+
+  auto *pair_cut = new PairPlotterCut(*static_cast<AliFemtoPairCutPionPionAKDetaDphi*>(analysis->PairCut()));
+  analysis->SetPairCut(pair_cut);
+
   // analysis->
-  analysis->AddCorrFctn(new AliFoo());
+  // analysis->AddCorrFctn(new AliFoo());
   mgr.AddAnalysis(analysis);
 }
 
