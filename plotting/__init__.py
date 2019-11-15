e126d93af7a29013482539960eac299ea30e9ca2
Andrew Kubera <andrew.kubera@cern.ch>
2019-11-15 17:51:30 -0500

    PlotData: Delegate attributes to canvas and add color_loader method

--- plotting/__init__.py
+++ plotting/__init__.py
@@ -8,20 +8,37 @@ import numpy as np
 
 
 class PlotData:
-    
+
     def __init__(self, c=None):
         self.canvas = c
-    
-    def Draw(self):
-        self.canvas.Draw()
-    
+
+    def __getattr__(self, key):
+        return getattr(self.canvas, key)
+
+    def color_loader(self, palette):
+        from ROOT import TColor
+        import seaborn as sns
+        colors = sns.color_palette(palette)
+        self.tcolors = [TColor(*rgb) for rgb in colors]
+        self.colors = [tcolor.GetNumber() for tcolor in self.tcolors]
+
+        color_it = iter(self.colors)
+
+        centrality_colors = {}
+
+        def centrality_color(cent):
+            if cent not in centrality_colors:
+                centrality_colors[cent] = next(color_it)
+            return centrality_colors[cent]
+
+        return centrality_color
+
 
 class FitData:
-    
+
     def __init__(self, path):
         self.data = json.loads(Path(path).read_text())
         self.df = pd.DataFrame(self.data['df'])
-
 
 
 def canvas_divide(c, *, rows=2, cols=3, rmargin=0, lmargin=0, tmargin=0):
@@ -31,17 +48,17 @@ def canvas_divide(c, *, rows=2, cols=3, rmargin=0, lmargin=0, tmargin=0):
 
     rect = c.GetBBox()
     # print(rect.fX, rect.fY, rect.fWidth, rect.fHeight)
-    
+
     c.Divide(cols, rows)
-    
+
     pad = c.cd(1)
     rect = pad.GetBBox()
     print(rect.fX, rect.fY, rect.fWidth, rect.fHeight)
-    
+
 #     pad.SetBBoxX1(0)
     pad.SetLeftMargin(3)
     print(rect.fX, rect.fY, rect.fWidth, rect.fHeight)
-    
+
 #     pad = TPad("", "", 0, 0.3, 0.1, 0.2)
     c.SetFillColor(ROOT.kRed)
 #     pad.Draw()
@@ -54,9 +71,9 @@ def canvas_divide(c, *, rows=2, cols=3, rmargin=0, lmargin=0, tmargin=0):
     for i in range(cols*rows):
         row, col = i // cols, i % cols
         pad = c.cd(i+1)
-        
+
         if i == 0:
-#             pad = 
+#             pad =
 #             continue
             rect = pad.GetBBox()
             center = pad.GetBBoxCenter()
@@ -65,34 +82,32 @@ def canvas_divide(c, *, rows=2, cols=3, rmargin=0, lmargin=0, tmargin=0):
             x2 = 26
             y1 = 0
             y2 = 100
-        
+
 #             pad.SetBBoxX1(x1)
             pad.SetBBoxX2(x2)
 #             pad.SetBBoxY1(y1)
 #             pad.SetBBoxY2(y2)
-            
+
             rect = pad.GetBBox()
             print(rect.fX, rect.fY, rect.fWidth, rect.fHeight)
-            
+
 #             pad.SetBBoxCenterX(50)
             pad.SetBBoxCenterY(0)
-            
+
             continue
-        
+
 #         pad.SetBBoxCenter(3 * col + 9)
         x1 = col * 100 // (cols+1)
         x2 = (col+1) * 100 // (cols+1)
         y1 = 0
         y2 = 100
-        
+
         pad.SetBBoxX1(x1)
         pad.SetBBoxX2(x2)
         pad.SetBBoxY1(y1)
         pad.SetBBoxY2(y2)
-        
+
 #         pad.SetBBoxX2(10 * col + 1)
-        
+
 #         if i > 1:
 #             break
-    
-    
