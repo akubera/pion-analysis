0eb035944b1bfcbeeed1254d52c3e9a5a4234000
Andrew Kubera <andrew.kubera@cern.ch>
2019-11-19 20:36:49 -0500

    plotting.systematics: Implement weighted standard deviation

--- plotting/systematics.py
+++ plotting/systematics.py
@@ -4,11 +4,24 @@
 
 
 import pandas as pd
-from plotting import PlotData
 
 
+def calc_df_systematics(df, keys):
 
-def plot_1d_levy(df: pd.DataFrame):
-    Rinv = df[['radius', '']]
-    return Rinv
-    
+    def _calc_sys_err(df, k):
+        return calc_weighted_variance(df[k], df[k + "_err"])
+
+    return {k: _calc_sys_err(df, k) for k in keys}
+
+
+def calc_weighted_variance(vals, errs):
+    weights = errs ** -2
+
+    mean = (vals * weights).sum() / weights.sum()
+
+    M = (weights > 0).sum()
+
+    # res_1 = (weights * (vals - mean) ** 2).sum() / ((M - 1) / M * weightssum())
+    res = (weights * (vals - mean) ** 2).sum() / (weights.sum() - (weights**2).sum() / weights.sum())
+    # print(pd.np.sqrt(res), pd.np.sqrt(res_1), vals.std())
+    return pd.np.sqrt(res)
